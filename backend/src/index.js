394ccb4f20c42fe33c3c1e646882de785123dac2
maaurisio <mauriciomrla@gmail.com>
2026-04-09 16:49:16 -0500

    variables de entorno +3

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port = process.env.port;
+const port = process.env.PORT || 3000;
 const jwtSecret = process.env.JWT_SECRET;
 
 app.use(express.json());
